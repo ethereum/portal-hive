397ed7f52ca69f32ddf45ffc796d5ca155bae6d3
Ognyan Genev <ognian.genev@gmail.com>
2022-11-24 13:05:58 +0200

    Expose Trin and Fluffy rpc addresses

--- clients/trin/trin.sh
+++ clients/trin/trin.sh
@@ -3,4 +3,4 @@
 # Immediately abort the script on any error encountered
 set -e
 
-RUST_LOG=debug TRIN_INFURA_PROJECT_ID="your-key-here" ./trin-main --web3-transport http
+RUST_LOG=debug TRIN_INFURA_PROJECT_ID="your-key-here" ./trin-main --web3-transport http --web3-http-address 0.0.0.0:8545
