ccb8bdb98a3b62f02bee25ca554652f21355b876
Ognyan Genev <ognian.genev@gmail.com>
2022-12-03 13:44:18 +0200

    clients: Bind to container IP address

--- clients/trin/trin.sh
+++ clients/trin/trin.sh
@@ -3,4 +3,6 @@
 # Immediately abort the script on any error encountered
 set -e
 
-RUST_LOG=debug TRIN_INFURA_PROJECT_ID="your-key-here" ./trin-main --web3-transport http --web3-http-address 0.0.0.0:8545
+IP_ADDR=$(hostname -i | awk '{print $1}')
+
+RUST_LOG=debug TRIN_INFURA_PROJECT_ID="your-key-here" ./trin-main --web3-transport http --web3-http-address 0.0.0.0:8545 --external-address "$IP_ADDR":9000
