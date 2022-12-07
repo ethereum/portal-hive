bda88d122bfdfd6ab3d53c437130ceb9a45b03c6
Ognyan Genev <ognian.genev@gmail.com>
2022-12-07 11:48:21 +0200

    Make hive go tests pass

--- internal/libhive/inventory_test.go
+++ internal/libhive/inventory_test.go
@@ -32,19 +32,16 @@ func TestInventory(t *testing.T) {
 	}
 
 	t.Run("HasClient", func(t *testing.T) {
-		if !inv.HasClient("go-ethereum") {
-			t.Error("can't find go-ethereum client")
-		}
-		if !inv.HasClient("go-ethereum_latest") {
-			t.Error("can't find go-ethereum_latest client")
+		if !inv.HasClient("trin") {
+			t.Error("can't find trin client")
 		}
 		if inv.HasClient("supereth3000") {
 			t.Error("returned true for unknown client")
 		}
 	})
 	t.Run("HasSimulator", func(t *testing.T) {
-		if !inv.HasSimulator("smoke/genesis") {
-			t.Error("can't find smoke/genesis simulator")
+		if !inv.HasSimulator("rpc-compat") {
+			t.Error("can't find rpc-compat simulator")
 		}
 		if inv.HasSimulator("unknown simulator name") {
 			t.Error("returned true for unknown simulator name")
