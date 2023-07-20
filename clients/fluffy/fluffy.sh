276e887e6e386c742f45dd9efc79a63df8816103
Kim De Mey <kim.demey@gmail.com>
2023-07-20 13:44:35 +0200

    Adjust fluffy start script to select no default bootstrap nodes (#66)

--- clients/fluffy/fluffy.sh
+++ clients/fluffy/fluffy.sh
@@ -5,4 +5,4 @@ set -e
 
 IP_ADDR=$(hostname -i | awk '{print $1}')
 
-fluffy --rpc --rpc-address="0.0.0.0" --nat:extip:"$IP_ADDR" --log-level="debug"
+fluffy --rpc --rpc-address="0.0.0.0" --nat:extip:"$IP_ADDR" --network=none --log-level="debug"
