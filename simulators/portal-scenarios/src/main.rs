88df9995863c8db81836f7a6db7c8373d308e4b6
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2023-07-22 12:24:49 -0600

    Add first 3 client test, for find_content

---
+++ simulators/portal-scenarios/src/main.rs
@@ -0,0 +1,212 @@
+use ethportal_api::jsonrpsee::core::__reexports::serde_json;
+use ethportal_api::types::portal::ContentInfo;
+use ethportal_api::types::distance::XorMetric;
+use ethportal_api::types::distance::Metric;
+use ethportal_api::{Discv5ApiClient, HistoryContentKey, HistoryContentValue, HistoryNetworkApiClient, OverlayContentKey};
+use hivesim::{dyn_async, Client, NClientTestSpec, Simulation, Suite, Test, TestSpec};
+use itertools::Itertools;
+use serde_json::json;
+
+// Header with proof for block number 14764013
+const HEADER_WITH_PROOF_KEY: &str =
+    "0x00720704f3aa11c53cf344ea069db95cecb81ad7453c8f276b2a1062979611f09c";
+const HEADER_WITH_PROOF_VALUE: &str = "0x080000002d020000f90222a02c58e3212c085178dbb1277e2f3c24b3f451267a75a234945c1581af639f4a7aa058a694212e0416353a4d3865ccf475496b55af3a3d3b002057000741af9731919400192fb10df37c9fb26829eb2cc623cd1bf599e8a067a9fb631f4579f9015ef3c6f1f3830dfa2dc08afe156f750e90022134b9ebf6a018a2978fc62cd1a23e90de920af68c0c3af3330327927cda4c005faccefb5ce7a0168a3827607627e781941dc777737fc4b6beb69a8b139240b881992b35b854eab9010000200000400000001000400080080000000000010004010001000008000000002000110000000000000090020001110402008000080208040010000000a8000000000000000000210822000900205020000000000160020020000400800040000000000042080000000400004008084020001000001004004000001000000000000001000000110000040000010200844040048101000008002000404810082002800000108020000200408008000100000000000000002020000b00010080600902000200000050000400000000000000400000002002101000000a00002000003420000800400000020100002000000000000000c00040000001000000100187327bd7ad3116ce83e147ed8401c9c36483140db184627d9afa9a457468657265756d50504c4e532f326d696e6572735f55534133a0f1a32e24eb62f01ec3f2b3b5893f7be9062fbf5482bc0d490a54352240350e26882087fbb243327696851aae1651b6010cc53ffa2df1bae1550a0000000000000000000000000000000000000000000063d45d0a2242d35484f289108b3c80cccf943005db0db6c67ffea4c4a47fd529f64d74fa6068a3fd89a2c0d9938c3a751c4706d0b0e8f99dec6b517cf12809cb413795c8c678b3171303ddce2fa1a91af6a0961b9db72750d4d5ea7d5103d8d25f23f522d9af4c13fe8ac7a7d9d64bb08d980281eea5298b93cb1085fedc19d4c60afdd52d116cfad030cf4223e50afa8031154a2263c76eb08b96b5b8fdf5e5c30825d5c918eefb89daaf0e8573f20643614d9843a1817b6186074e4e53b22cf49046d977c901ec00aef1555fa89468adc2a51a081f186c995153d1cba0f2887d585212d68be4b958d309fbe611abe98a9bfc3f4b7a7b72bb881b888d89a04ecfe08b1c1a48554a48328646e4f864fe722f12d850f0be29e3829d1f94b34083032a9b6f43abd559785c996229f8e022d4cd6dcde4aafcce6445fe8743e1fcbe8672a99f9d9e3a5ca10c01f3751d69fbd22197f0680bc1529151130b22759bf185f4dbce357f46eb9cc8e21ea78f49b298eea2756d761fe23de8bea0d2e15aed136d689f6d252c54ebadc3e46b84a397b681edf7ec63522b9a298301084d019d0020000000000000000000000000000000000000000000000000000000000000";
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let mut suite = Suite {
+        name: "portal-scenarios".to_string(),
+        description:
+            "The portal scenarios test suite runs a set of scenarios to test 3 or more clients"
+                .to_string(),
+        tests: vec![],
+    };
+
+    suite.add(TestSpec {
+        name: "Portal Network scenarios".to_string(),
+        description: "".to_string(),
+        always_run: false,
+        run: test_portal_scenarios,
+        client: None,
+    });
+
+    let sim = Simulation::new();
+    run_suite(sim, suite).await;
+}
+
+async fn run_suite(host: Simulation, suite: Suite) {
+    let name = suite.clone().name;
+    let description = suite.clone().description;
+
+    let suite_id = host.start_suite(name, description, "".to_string()).await;
+
+    for test in &suite.tests {
+        test.run_test(host.clone(), suite_id, suite.clone()).await;
+    }
+
+    host.end_suite(suite_id).await;
+}
+
+dyn_async! {
+   async fn test_portal_scenarios<'a> (test: &'a mut Test, _client: Option<Client>) {
+        // Get all available portal clients
+        let clients = test.sim.client_types().await;
+
+        // Iterate over all possible pairings of clients and run the tests (including self-pairings)
+        for ((client_a, client_b), client_c) in clients.iter().cartesian_product(clients.iter()).cartesian_product(clients.iter()) {
+
+            // Test block header with proof
+            test.run(
+                NClientTestSpec {
+                    name: format!("FIND_CONTENT content stored 2 nodes away {} --> {} --> {}", client_a.name, client_b.name, client_c.name),
+                    description: "".to_string(),
+                    always_run: false,
+                    run: test_find_content_two_jumps,
+                    clients: &vec![&(*client_a).clone(), &(*client_b).clone(), &(*client_c).clone()],
+                }
+            ).await;
+        }
+   }
+}
+
+dyn_async! {
+    // test that a node will return content via FIND_CONTENT that is stored 2 nodes away
+    async fn test_find_content_two_jumps<'a> (test: &'a mut Test, clients: Vec<Client>) {
+        let (client_a, client_b, client_c) = match clients.iter().collect_tuple() {
+            Some((client_a, client_b, client_c)) => (client_a, client_b, client_c),
+            None => {
+                test.fatal("Unable to get expected amount of clients from NClientTestSpec");
+                return;
+            }
+        };
+
+        let header_with_proof_key: HistoryContentKey = serde_json::from_value(json!(HEADER_WITH_PROOF_KEY)).unwrap();
+        let header_with_proof_value: HistoryContentValue = serde_json::from_value(json!(HEADER_WITH_PROOF_VALUE)).unwrap();
+
+        /// todo: remove for testing
+        let client_a_enr = match client_a.rpc.node_info().await {
+            Ok(node_info) => node_info.enr,
+            Err(err) => {
+                test.fatal(&format!("Error getting node info: {err:?}"));
+                return;
+            }
+        };
+
+        // get enr for b and c to seed for the jumps
+        let client_b_enr = match client_b.rpc.node_info().await {
+            Ok(node_info) => node_info.enr,
+            Err(err) => {
+                test.fatal(&format!("Error getting node info: {err:?}"));
+                return;
+            }
+        };
+
+        let client_c_enr = match client_c.rpc.node_info().await {
+            Ok(node_info) => node_info.enr,
+            Err(err) => {
+                test.fatal(&format!("Error getting node info: {err:?}"));
+                return;
+            }
+        };
+
+        let ddebug = format!("distances a {} :: b {} :: c {} from content",
+            XorMetric::distance(&client_a_enr.node_id().raw(), &header_with_proof_key.clone().content_id()),
+            XorMetric::distance(&client_b_enr.node_id().raw(), &header_with_proof_key.clone().content_id()),
+            XorMetric::distance(&client_c_enr.node_id().raw(), &header_with_proof_key.clone().content_id()),
+            );
+
+        // seed the data into client_c
+        match client_c.rpc.store(header_with_proof_key.clone(), header_with_proof_value.clone()).await {
+            Ok(result) => if !result {
+                test.fatal("Unable to store header with proof for find content immediate return test");
+                return;
+            },
+            Err(err) => {
+                test.fatal(&format!("Error storing header with proof for find content immediate return test: {err:?}"));
+                return;
+            }
+        }
+
+
+
+        /// todo: remove for testing
+        let debug_enrs = format!(" client a {:?}, client b {:?}, client c {:?} ", client_a_enr.clone(), client_b_enr.clone(), client_c_enr.clone());
+
+        // seed client_c_enr into routing table of client_b
+        match HistoryNetworkApiClient::add_enr(&client_b.rpc, client_c_enr.clone()).await {
+            Ok(response) => match response {
+                true => (),
+                false => test.fatal("AddEnr expected to get true and instead got false")
+            },
+            Err(err) => test.fatal(&err.to_string()),
+        }
+
+        // check if we can fetch data from routing table
+        match HistoryNetworkApiClient::get_enr(&client_b.rpc, client_c_enr.node_id()).await {
+            Ok(response) => {
+                if response != client_c_enr.clone() {
+                    test.fatal("Response from GetEnr didn't return expected Enr");
+                     return;
+                }
+            },
+            Err(err) => test.fatal(&err.to_string()),
+        }
+
+        // check if we can fetch data from routing table
+        match HistoryNetworkApiClient::get_enr(&client_c.rpc, client_b_enr.node_id()).await {
+            Ok(response) => {
+                if response != client_b_enr.clone() {
+                    test.fatal("Response from GetEnr didn't return expected Enr");
+                    return;
+                }
+            },
+            Err(err) => test.fatal(&err.to_string()),
+        }
+
+        let result = client_a.rpc.find_content(client_b_enr, header_with_proof_key.clone()).await;
+
+        let enrs = match result {
+            Ok(result) => {
+                match result {
+                    ContentInfo::Enrs{ enrs } => {
+                        enrs
+                    },
+                    other => {
+                        test.fatal(&format!("Error: Unexpected FINDCONTENT response not Enrs: {other:?}"));
+                        return;
+                    }
+                }
+            },
+            Err(err) => {
+                test.fatal(&format!("Error: Unable to get response from FINDCONTENT request (Enrs): {err:?} distances {ddebug}"));
+                return;
+            }
+        };
+
+        if enrs.len() != 1 {
+            test.fatal(&format!("The only enr this should return is client c: length {} {:?} :: clients {}", enrs.len(), enrs.clone(),  debug_enrs));
+        }
+
+        let result = client_a.rpc.find_content(enrs[0].clone(), header_with_proof_key.clone()).await;
+
+        match result {
+            Ok(result) => {
+                match result {
+                    ContentInfo::Content{ content: val } => {
+                        if val != header_with_proof_value {
+                            test.fatal("Error: Unexpected FINDCONTENT response: didn't return expected header with proof value");
+                        }
+                    },
+                    other => {
+                        test.fatal(&format!("Error: Unexpected FINDCONTENT response: {other:?}"));
+                    }
+                }
+            },
+            Err(err) => {
+                test.fatal(&format!("Error: Unable to get response from FINDCONTENT request: {err:?}"));
+            }
+        }
+    }
+}
