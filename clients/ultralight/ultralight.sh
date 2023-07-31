9d738ce776787ff5b0b6fc5d0a53abf84dbac8cb
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2023-07-31 08:22:41 -0600

    Add first 3 client tests, for find_content and choosing private key option (#61)
    * Add first 3 client test, for find_content
    
    * Update Cargo.lock
    
    * update test to panic!
    
    * Implemented test that works
    
    * Add private keys section
    
    * Update main.rs
    
    * Resolve PR comments

--- clients/ultralight/ultralight.sh
+++ clients/ultralight/ultralight.sh
@@ -5,4 +5,9 @@ set -e
 
 IP_ADDR=$(hostname -i | awk '{print $1}')
 
-node /ultralight/packages/cli/dist/index.js --bindAddress="$IP_ADDR:9000" --dataDir="./data" --rpcPort=8545
+# if CLIENT_PRIVATE_KEY isn't set or doesn't exist do y, else do z
+if [ -z ${CLIENT_PRIVATE_KEY+x} ]; then
+  node /ultralight/packages/cli/dist/index.js --bindAddress="$IP_ADDR:9000" --dataDir="./data" --rpcPort=8545
+else
+  node /ultralight/packages/cli/dist/index.js --bindAddress="$IP_ADDR:9000" --dataDir="./data" --rpcPort=8545 --pk=${CLIENT_PRIVATE_KEY}
+fi
