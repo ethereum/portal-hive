4b97046a947fcbbbc3373e7daaa001515eb6ecd7
ScottyPoi <scott.simpson@ethereum.org>
2023-08-14 15:57:36 -0600

    point at working branch ultralight

--- clients/ultralight/ultralight.sh
+++ clients/ultralight/ultralight.sh
@@ -5,4 +5,4 @@ set -e
 
 IP_ADDR=$(hostname -i | awk '{print $1}')
 
-node /ultralight/packages/cli/dist/index.js --bindAddress="$IP_ADDR:9000" --dataDir="./data" --rpcPort=8545
+DEBUG=* node /ultralight/packages/cli/dist/index.js --bindAddress="$IP_ADDR:9000" --dataDir="./data" --rpcPort=8545
