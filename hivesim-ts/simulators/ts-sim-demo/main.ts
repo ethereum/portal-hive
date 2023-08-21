262754f5c165206580874d76a3fb08c0a81abd68
ScottyPoi <scott.simpson@ethereum.org>
2023-08-21 14:36:32 -0600

    add 2 client test to demo

--- hivesim-ts/simulators/ts-sim-demo/main.ts
+++ hivesim-ts/simulators/ts-sim-demo/main.ts
@@ -1,5 +1,5 @@
 import { Simulation } from "../../simulation.js";
-import { ClientTestSpec, IClient, Suite, Test, TestSpec } from "../../testapi.js";
+import { ClientTestSpec, IClient, Suite, Test, TestSpec, TwoClientTestSpec } from "../../testapi.js";
 import { decodeENR } from "../../utils.js";
 
 const client_enr_tag = async (test: Test, client: IClient) => {
@@ -15,17 +15,43 @@ const client_enr_tag = async (test: Test, client: IClient) => {
     if (!nodeInfo.enr || !nodeInfo.nodeId) {
         test.fatal(`Expected response not received: ${res.error}`) 
     }
+    const nodeId = nodeInfo.nodeId
     if (clients[client.kind] !== (enr.c).slice(0,7)) {
         test.fatal(`Expected client type ${clients[client.kind]}, got ${enr.c}`)
     }
 }
 
-const run_all_client_tests = async (test: Test, _client?: IClient) => {
+const two_client_demo = async (test: Test, client_a: IClient, client_b: IClient) => {
+    const res1 = await client_a.rpc.request("discv5_nodeInfo", [])
+    const res2 = await client_b.rpc.request("discv5_nodeInfo", [])
+    const nodeInfo1 = res1.result
+    const nodeInfo2 = res2.result
+    const ping1 = await client_a.rpc.request("portal_historyPing", [nodeInfo2.enr])
+    const ping2 = await client_b.rpc.request("portal_historyPing", [nodeInfo1.enr])
+    console.log({
+        nodeInfo1,
+        nodeInfo2,
+        ping1: ping1.result,
+        ping2: ping2.result
+
+    })
+    if (!ping1.result || !ping2.result) {
+        test.fatal(`Expected response not received: ${ping1.error} ${ping2.error}`)
+    }
+}
+
+const run_all_client_tests = async (test: Test) => {
     await test.run(new ClientTestSpec({
         name: "discv5_nodeInfo",
         description: "returns the node_id and ENR with correct client tag",
         always_run: true,
         run: client_enr_tag,
+    }))
+    await test.run(new TwoClientTestSpec({
+        name: "portal_historyPing",
+        description: "returns the node_id and ENR with correct client tag",
+        always_run: true,
+        run: two_client_demo,
     }))
 // run other tests...
 }
