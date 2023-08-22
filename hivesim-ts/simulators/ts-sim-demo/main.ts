94085a9d6be3375626b8fed1311442ea89df8b64
ScottyPoi <scott.simpson@ethereum.org>
2023-08-21 18:37:03 -0600

    add n_client demo test

--- hivesim-ts/simulators/ts-sim-demo/main.ts
+++ hivesim-ts/simulators/ts-sim-demo/main.ts
@@ -1,78 +1,149 @@
 import { Simulation } from "../../simulation.js";
-import { ClientTestSpec, IClient, Suite, Test, TestSpec, TwoClientTestSpec } from "../../testapi.js";
+import {
+  ClientTestSpec,
+  IClient,
+  NClientTestSpec,
+  Suite,
+  Test,
+  TestSpec,
+  TwoClientTestSpec,
+} from "../../testapi.js";
 import { decodeENR } from "../../utils.js";
 
 const client_enr_tag = async (test: Test, client: IClient) => {
-    const clients: any = {
-        'trin': 't 0.1.1',
-        'fluffy': 'f 0.0.1',
-        'ultralight': 'u 0.0.1'
-    }
-    
-    const res = await client.rpc.request("discv5_nodeInfo", [])
-    const nodeInfo = res.result
-    const enr = decodeENR(nodeInfo.enr)
-    if (!nodeInfo.enr || !nodeInfo.nodeId) {
-        test.fatal(`Expected response not received: ${res.error}`) 
-    }
-    const nodeId = nodeInfo.nodeId
-    if (clients[client.kind] !== (enr.c).slice(0,7)) {
-        test.fatal(`Expected client type ${clients[client.kind]}, got ${enr.c}`)
-    }
-}
+  const clients: any = {
+    trin: "t 0.1.1",
+    fluffy: "f 0.0.1",
+    ultralight: "u 0.0.1",
+  };
 
-const two_client_demo = async (test: Test, client_a: IClient, client_b: IClient) => {
-    const res1 = await client_a.rpc.request("discv5_nodeInfo", [])
-    const res2 = await client_b.rpc.request("discv5_nodeInfo", [])
-    const nodeInfo1 = res1.result
-    const nodeInfo2 = res2.result
-    const ping1 = await client_a.rpc.request("portal_historyPing", [nodeInfo2.enr])
-    const ping2 = await client_b.rpc.request("portal_historyPing", [nodeInfo1.enr])
-    console.log({
-        nodeInfo1,
-        nodeInfo2,
-        ping1: ping1.result,
-        ping2: ping2.result
+  const res = await client.rpc.request("discv5_nodeInfo", []);
+  const nodeInfo = res.result;
+  const enr = decodeENR(nodeInfo.enr);
+  if (!nodeInfo.enr || !nodeInfo.nodeId) {
+    test.fatal(`Expected response not received: ${res.error}`);
+  }
+  const nodeId = nodeInfo.nodeId;
+  if (clients[client.kind] !== enr.c.slice(0, 7)) {
+    test.fatal(`Expected client type ${clients[client.kind]}, got ${enr.c}`);
+  }
+};
 
+const two_client_demo = async (
+  test: Test,
+  client_a: IClient,
+  client_b: IClient
+) => {
+  const res1 = await client_a.rpc.request("discv5_nodeInfo", []);
+  const res2 = await client_b.rpc.request("discv5_nodeInfo", []);
+  const nodeInfo1 = res1.result;
+  const nodeInfo2 = res2.result;
+  const ping1 = await client_a.rpc.request("portal_historyPing", [
+    nodeInfo2.enr,
+  ]);
+  const ping2 = await client_b.rpc.request("portal_historyPing", [
+    nodeInfo1.enr,
+  ]);
+  console.log({
+    nodeInfo1,
+    nodeInfo2,
+    ping1: ping1.result,
+    ping2: ping2.result,
+  });
+  if (!ping1.result || !ping2.result) {
+    test.fatal(`Expected response not received: ${ping1.error} ${ping2.error}`);
+  }
+};
+
+const n_client_demo = async (test: Test, clients: IClient[]) => {
+  const clientsInfo = await Promise.all(
+    clients.map(async (client) => {
+      const res = await client.rpc.request("discv5_nodeInfo", []);
+      return res.result;
     })
-    if (!ping1.result || !ping2.result) {
-        test.fatal(`Expected response not received: ${ping1.error} ${ping2.error}`)
+  );
+  console.log(clientsInfo);
+  if (clientsInfo.some((info) => !info)) {
+    test.fatal(
+      `Expected response not received: ${clientsInfo.map((info) => info.error)}`
+    );
+  }
+  for await (const [i, client] of clients.entries()) {
+    const res = await client.rpc.request("portal_historyPing", [
+      clientsInfo[(i + 1) % clients.length].enr,
+    ]);
+    if (!res.result) {
+      test.fatal(`Expected response not received`);
     }
-}
+  }
+  const routingTables = await Promise.all(
+    clients.map(async (client) => {
+      const res = await client.rpc.request(
+        "portal_historyRoutingTableInfo",
+        []
+      );
+      return res.result;
+    })
+  );
+  console.log(routingTables);
+    if (routingTables.some((table) => !table)) {
+      test.fatal(
+        `Expected response not received: ${routingTables.map(
+          (table) => table.error
+        )}`
+      );
+    }
+    for (const table of routingTables) {
+      if (clientsInfo.some((info) => !table.includes(info.nodeId))) {
+        test.fatal(`Expected routing table to include all nodes, got ${table}`);
+      }
+    }
+};
 
 const run_all_client_tests = async (test: Test) => {
-    await test.run(new ClientTestSpec({
-        name: "discv5_nodeInfo",
-        description: "returns the node_id and ENR with correct client tag",
-        always_run: true,
-        run: client_enr_tag,
-    }))
-    await test.run(new TwoClientTestSpec({
-        name: "portal_historyPing",
-        description: "returns the node_id and ENR with correct client tag",
-        always_run: true,
-        run: two_client_demo,
-    }))
-// run other tests...
-}
+  await test.run(
+    new ClientTestSpec({
+      name: "discv5_nodeInfo",
+      description: "returns the node_id and ENR with correct client tag",
+      always_run: true,
+      run: client_enr_tag,
+    })
+  );
+  await test.run(
+    new TwoClientTestSpec({
+      name: "portal_historyPing",
+      description: "returns the node_id and ENR with correct client tag",
+      always_run: true,
+      run: two_client_demo,
+    })
+  );
+  await test.run(
+    new NClientTestSpec({
+      name: "portal_historyRoutingTableInfo",
+      description: "all clients should connect",
+      always_run: true,
+      run: n_client_demo,
+    })
+  );
+  // run other tests...
+};
 
 const main = async () => {
-    const suite = new Suite(
-        "ts-sim-demo",
-        "The RPC-compatibility test suite runs a set of RPC related tests against a running node. It tests client implementations of the JSON-RPC API for conformance with the portal network API specification.",
-        )
-    suite.add(
-        new TestSpec({
-            name: "client launch",
-            description: "This test launches the client and collects its logs.",
-            always_run: true,
-            run: run_all_client_tests,
-        })
-    )
+  const suite = new Suite(
+    "ts-sim-demo",
+    "The RPC-compatibility test suite runs a set of RPC related tests against a running node. It tests client implementations of the JSON-RPC API for conformance with the portal network API specification."
+  );
+  suite.add(
+    new TestSpec({
+      name: "client launch",
+      description: "This test launches the client and collects its logs.",
+      always_run: true,
+      run: run_all_client_tests,
+    })
+  );
 
-        const sim = new Simulation(`${process.env['HIVE_SIMULATOR']}`)
-        await suite.run(sim)
+  const sim = new Simulation(`${process.env["HIVE_SIMULATOR"]}`);
+  await suite.run(sim);
+};
 
-}
-
-main()
+main();
