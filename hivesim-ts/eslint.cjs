8fcce79c92960b72ade44671de31ee916d23a7fa
ScottyPoi <scott.simpson@ethereum.org>
2023-08-31 01:43:28 -0600

    setup lint config

---
+++ hivesim-ts/eslint.cjs
@@ -0,0 +1,56 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'implicit-dependencies', 'prettier'],
+  env: {
+    es2020: true,
+    node: true,
+  },
+  parserOptions: {},
+  ignorePatterns: ['node_modules/', 'dist/', 'prettier.config.cjs'],
+  extends: ['eslint:recommended', 'plugin:node/recommended'],
+  rules: {
+    'node/file-extension-in-import': [
+      {
+        tryExtensions: ['.js', '.json', '.node'],
+      },
+    ],
+    'node/no-unsupported-features/es-syntax': 'off',
+    'no-undef': 'warn',
+    'no-process-exit': 'off',
+    'node/no-unpublished-import': 'off',
+    'node/no-missing-import': 'off',
+    'no-empty': 'off',
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'implicit-dependencies/no-implicit': ['error', { peer: true, dev: true, optional: true }],
+    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    '@typescript-eslint/no-use-before-define': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase', 'camelCase'],
+      },
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/no-unnecessary-condition': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'no-dupe-class-members': 'off',
+    'no-extra-semi': 'off',
+    'prettier/prettier': 'error',
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': ['error'],
+    'no-case-declarations': 'warn',
+    'node/file-extension-in-import': ['error', 'always'],
+  },
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.lint.json',
+  },
+}
