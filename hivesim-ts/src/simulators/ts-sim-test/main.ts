b3a08fe6391f82b31b8778a98cea64d980c85723
ScottyPoi <scott.simpson@ethereum.org>
2023-12-06 01:15:59 -0700

    update ts-sim-test

--- hivesim-ts/src/simulators/ts-sim-test/main.ts
+++ hivesim-ts/src/simulators/ts-sim-test/main.ts
@@ -11,20 +11,14 @@ import {
 import { decodeENR } from '../../utils.js'
 
 const client_enr_tag = async (test: Test, client: IClient) => {
-  const clients: any = {
-    trin: 't 0.1.1',
-    fluffy: 'f 0.0.1',
-    ultralight: 'u 0.0.1',
-  }
-
   const res = await client.rpc.request('discv5_nodeInfo', [])
   const nodeInfo = res.result
   const enr = decodeENR(nodeInfo.enr)
   if (!nodeInfo.enr || !nodeInfo.nodeId) {
     test.fatal(`Expected response not received: ${res.error}`)
   }
-  if (clients[client.kind] !== enr.c.slice(0, 7)) {
-    test.fatal(`Expected client type ${clients[client.kind]}, got ${enr.c}`)
+  if (client.kind[0] !== enr.c[0]) {
+    test.fatal(`Expected client type to begin with: ${client.kind[0]}, got ${enr.c}`)
   }
 }
 
@@ -33,8 +27,15 @@ const two_client_demo = async (test: Test, client_a: IClient, client_b: IClient)
   const res2 = await client_b.rpc.request('discv5_nodeInfo', [])
   const nodeInfo1 = res1.result
   const nodeInfo2 = res2.result
+  if (!nodeInfo1.enr || !nodeInfo1.nodeId || !nodeInfo2.enr || !nodeInfo2.nodeId) {
+    test.fatal(`Expected response not received: \n ${res1} \n ${res2}`)
+  }
   const ping1 = await client_a.rpc.request('portal_historyPing', [nodeInfo2.enr])
   const ping2 = await client_b.rpc.request('portal_historyPing', [nodeInfo1.enr])
+  console.log({
+    ping1,
+    ping2,
+  })
   if (!ping1.result || !ping2.result) {
     test.fatal(`Expected response not received: ${ping1.error} ${ping2.error}`)
   }
@@ -53,34 +54,49 @@ const n_client_demo = async (test: Test, clients: IClient[]) => {
     }
   }
   for await (const [i, client] of clients.entries()) {
-    const res = await client.rpc.request('portal_historyPing', [
-      clientsInfo[(i + 1) % clients.length][1].enr,
-    ])
-    if (!res.result) {
-      test.fatal(`${clientsInfo[(i + 1) % clients.length][0]}: Expected response not received`)
+    for await (const [_i, _client] of clients.entries()) {
+      if (i === _i) {
+        continue
+      }
+      const res = await client.rpc.request('portal_historyPing', [clientsInfo[_i][1].enr])
+      if (!res.result) {
+        test.fatal(`ping {${client} --> ${clientsInfo[_i]}}: Expected response not received`)
+      }
     }
   }
+  await new Promise((resolve) => setTimeout(resolve, 1000))
   const routingTables = await Promise.all(
     clients.map(async (client) => {
       const res = await client.rpc.request('portal_historyRoutingTableInfo', [])
       return [client.kind, res.result]
     }),
   )
-  if (routingTables.some((table) => !table)) {
+  if (routingTables.some((table) => !table || !table[1].buckets || !table[1].localNodeId)) {
     test.fatal(`Expected response not received`)
   }
   const errors: string[] = []
+  const passing: string[] = []
   for (const [client, table] of routingTables) {
-    const expected = clientsInfo.map(([_, c]) => [_, c.nodeId]).filter(([k, _]) => k !== client)
-    const peers = Object.values(table.buckets).flat()
+    const expected = clientsInfo.filter(([k, _]) => k !== client).map(([_, c]) => c.nodeId)
+    const peers = Object.values(table.buckets).flat() as string[]
     if (peers.length < expected.length) {
       errors.push(
-        `${client}: expected ${JSON.stringify(expected)} peers, got ${JSON.stringify(peers)}`,
+        `${client}: ${peers.length} peers (fail)\n    expected: [${expected.join(
+          `,\n               `,
+        )}] \n         got: [${peers.join(
+          `,\n               `,
+        )}] \n(${client}): portal_historyRoutingTableInfo => ${JSON.stringify(table, null, 2)}`,
+      )
+    } else {
+      passing.push(
+        `${client}: ${peers.length} peers (pass)\n${' '.repeat(4)}  [${peers
+          .map((p) => `${p.startsWith('0x') ? `${p.slice(0, 10)}` : `0x${p.slice(0, 8)}`}...`)
+          .join(', ')}]`,
       )
     }
   }
   if (errors.length > 0) {
-    test.fatal(errors.join('\n'))
+    test.fatal([...passing, ...errors].join('\n'))
   }
 }
 
@@ -114,7 +130,7 @@ const run_all_client_tests = async (test: Test) => {
 
 const main = async () => {
   const suite = new Suite(
-    'ts-sim-demo',
+    'ts-sim-test',
     'The RPC-compatibility test suite runs a set of RPC related tests against a running node. It tests client implementations of the JSON-RPC API for conformance with the portal network API specification.',
   )
   suite.add(
