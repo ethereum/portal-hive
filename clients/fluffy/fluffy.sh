370f7b4427bc963e220882ca6cb91c225d866dbd
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-01-16 08:50:32 -0700

    chore: clean up client bash files and add configurable networks (#125)

--- clients/fluffy/fluffy.sh
+++ clients/fluffy/fluffy.sh
@@ -4,10 +4,11 @@
 set -e
 
 IP_ADDR=$(hostname -i | awk '{print $1}')
+FLAGS=""
 
-# if HIVE_CLIENT_PRIVATE_KEY isn't set or doesn't exist do y, else do z
-if [ -z ${HIVE_CLIENT_PRIVATE_KEY+x} ]; then
-  fluffy --rpc --rpc-address="0.0.0.0" --nat:extip:"$IP_ADDR" --network=none --log-level="debug"
-else
-  fluffy --rpc --rpc-address="0.0.0.0" --nat:extip:"$IP_ADDR" --network=none --log-level="debug" --netkey-unsafe=0x${HIVE_CLIENT_PRIVATE_KEY}
+if [ "$HIVE_CLIENT_PRIVATE_KEY" != "" ]; then
+    FLAGS="$FLAGS --netkey-unsafe=0x$HIVE_CLIENT_PRIVATE_KEY"
 fi
+
+# Fluffy runs all networks by default, so we can not configure to run networks individually
+fluffy --rpc --rpc-address="0.0.0.0" --nat:extip:"$IP_ADDR" --network=none --log-level="debug" $FLAGS
