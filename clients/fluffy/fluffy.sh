603c4bf4785295e19cd09c2de42ed9dc04c63912
kdeme <kim.demey@gmail.com>
2024-01-29 10:48:28 +0100

    Enable beacon network for fluffy portal-hive tests

--- clients/fluffy/fluffy.sh
+++ clients/fluffy/fluffy.sh
@@ -4,7 +4,9 @@
 set -e
 
 IP_ADDR=$(hostname -i | awk '{print $1}')
-FLAGS=""
+# Providing atrusted block root is required currently to enable the beacon network.
+# It can be a made up value for now as tests are not doing any sync.
+FLAGS="--trusted-block-root:0x0000000000000000000000000000000000000000000000000000000000000000"
 
 if [ "$HIVE_CLIENT_PRIVATE_KEY" != "" ]; then
     FLAGS="$FLAGS --netkey-unsafe=0x$HIVE_CLIENT_PRIVATE_KEY"
